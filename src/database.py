956b07a25e65191baed3abd81e336ead8e76e745
Briana Lonappan <brianalonappan@gmail.com>
2025-06-02 21:33:31 -0700

    Updated

--- src/database.py
+++ src/database.py
@@ -1,5 +1,8 @@
-from src import config
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from src import config
+
+Base = declarative_base()
 
 connection_url = config.get_settings().POSTGRES_URI
 engine = create_engine(connection_url, pool_pre_ping=True)
